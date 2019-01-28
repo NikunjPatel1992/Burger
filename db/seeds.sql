3e8e5cafbc9dd9617ed53a176c80319d6e6ae198
nikunj <nikupatel199@gmail.com>
2019-01-28 15:57:03 -0600

    burger app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese burgers');
+INSERT INTO burgers (burger_name) VALUES ('chicken burgers');
+INSERT INTO burgers (burger_name, devoured) VALUES ('veg burgers', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('fish burgers', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('beef burgers', true);
+INSERT INTO burgers (burger_name) VALUES ('mac chicken burgers');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,106 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+  $(".devoured").on("click", function (event) {
+    event.preventDefault();
+    var devouredid = $(this).data("devouredid");
+    var newDevoured = true;
+    var newDevouredState = {
+      devoured: newDevoured
+    };
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + devouredid, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function () {
+        console.log("changed burger to", newDevoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).data("deleteid");
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function () {
+        console.log("delete id data");
+        // Reload the page to get the updated list
+        // location.reload();
+      }
+    );
+    location.reload();
+  });
+
+  $(".create-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burgername").val().trim()
+      // devoured: $("[name=devoured]:checked").val().trim()     
+    };
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function () {
+        console.log("created new Burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".select-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    console.log($(this).data("name"));
+    console.log($(this).data("id"));
+    // var newBurger = {
+    //   name: $("#burgername").val().trim()
+    //   // devoured: $("[name=devoured]:checked").val().trim()     
+    // };
+    // // Send the POST request.
+    // $.ajax("/api/burgers", {
+    //   type: "POST",
+    //   data: newBurger
+    // }).then(
+    //   function () {
+    //     console.log("created new Burger");
+    //     // Reload the page to get the updated list
+    //     location.reload();
+    //   }
+    // );
+  });
+
+
+  $(".update-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+    var id = $(this).data("id");
+    
+    var updateBurger = {
+      name: $("#burger").val().trim()
+      // devoured: $("[name=devoured]:checked").val().trim()     
+    };
+    // Send the POST request.
+    $.ajax("/api/burgersname/" + id , {
+      type: "PUT",
+      data: updateBurger
+    }).then(
+      function () {
+        console.log("update new Burger");
+        // Reload the page to get the updated list
+        location.assign("/");
+      }
+    );
+  });
+});
